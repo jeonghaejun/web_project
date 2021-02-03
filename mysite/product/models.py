8b4991cf94c2f7d786406a265d37b43ab9b07590
wjdgownsll <wjdgownsll@gmail.com>
2021-02-03 16:05:05 +0900

    21.02.03 web_project

--- mysite/product/models.py
+++ mysite/product/models.py
@@ -6,17 +6,26 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField('NAME', max_length=50)
     cpu = models.CharField('CPU', max_length=50)
-    ram = models.IntegerField('RAM')
+    ram = models.CharField('RAM', max_length=50)
     gpu = models.CharField('GPU', max_length=50)
-    battery=models.IntegerField('BATTERY')
-    volume = models.IntegerField('VOLUME')
+    battery = models.CharField('BATTERY', max_length=50)
+    volume = models.CharField('VOLUME', max_length=50)
     weight = models.DecimalField('WEIGHT',decimal_places=2,max_digits=4)
     display = models.DecimalField('DISPLAY', decimal_places=1,max_digits=3)
     maker = models.CharField('MAKER', max_length=50)
     sw_os = models.CharField('SW_OS', max_length=20, default='')
     price = models.IntegerField('PRICE')
-    image = models.ImageField('IMAGE',upload_to='product/', blank=True)
+    image = models.ImageField('IMAGE', blank=True)
     content = models.TextField('CONTENT')
     
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+
+        return url
